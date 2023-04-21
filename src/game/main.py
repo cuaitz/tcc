9a403accbb00bd7eb183ba55d80563cde95114a0
cuaitz <ocuait@gmail.com>
2023-04-20 22:53:45 -0300

    alterado o state inicial

--- src/game/main.py
+++ src/game/main.py
@@ -4,5 +4,5 @@ from . import states
 core.register_state('splash', states.splash_state)
 core.register_state('playing', states.playing_state)
 core.register_state('gameover', states.gameover_state)
-core.push_state('playing')
+core.push_state('splash')
 core.loop()
