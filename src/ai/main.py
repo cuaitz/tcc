a9168cacfa908da75c762a0108d94b1892979775
cuaitz <ocuait@gmail.com>
2023-05-27 02:27:04 -0300

    Alterado o modo de captura de tela
    Screenshots agora são tiradas com o módulo mss ("Multiple Screen Shots") ao invés do PIL, sendo drasticamente mais rápido;
    
    Adicionado uma função específica para tirar as screenshots;
    
    Alterado o nome dos parâmetros da função find_in_image para simplificar o entendimento, removido totalmente o parâmetro best_only e corrigido o retorno da função.

--- src/ai/main.py
+++ src/ai/main.py
@@ -2,8 +2,8 @@ import time
 import typing
 
 import cv2
+import mss
 import numpy as np
-from PIL import ImageGrab
 import pygame
 import win32api
 import win32con
@@ -25,49 +25,60 @@ def click(position: tuple[int, int]) -> None:
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
     time.sleep(.01)
 
+def take_screenshot(area: tuple[int, int, int, int]) -> np.array:
+    #  Captura a tela
+    screenshot = np.array(_screenshoter.grab(area))
+    
+    #  Converte a cor para o formato utilizado pelo cv2, BGR
+    return cv2.cvtColor(screenshot, cv2.COLOR_BGRA2BGR)
+
 def find_in_image(
-        needle: np.array,
-        haystack: np.array,
-        threshold: float = .999, 
-        best_only = False) -> typing.List[None | Rectangle]:
+    template: np.array,
+    area: np.array,
+    threshold: float = .999) -> list[float, Rectangle]:
     
-    result = cv2.matchTemplate(haystack, needle, cv2.TM_CCOEFF_NORMED)
+    #  Faz a busca do template na imagem
+    result = cv2.matchTemplate(area, template, cv2.TM_CCOEFF_NORMED)
     
-    #  Filtra os resultados menores que o mínimo e inverte a coord X e Y
-    matches = np.array(np.where(result >= threshold)[::-1])
+    #  Filtra os resultados menores que o mínimo
+    matches = np.where(result >= threshold)
     
-    height, width, _ = needle.shape  # needle.shape = (rows, columns, channels)
+    ...
+    
+    #  Inverte a coordenada X e Y
+    matches = matches[::-1]
+    
+    
+    height, width, _ = template.shape  # needle.shape = (rows, columns, channels)
     
     output = []
     for x, y in zip(*matches):
         output.append(
-            (result[y][x], Rectangle(x, y, width, height)
-            )
+            (result[y][x], Rectangle(x, y, width, height))
         )
     
-    if not output:
-        return output
+    #  Organiza pela similaridade de maneira decrescente
+    output.sort(key=lambda x: x[0], reverse=True)
     
-    output.sort(key=lambda x: x[0])
-    
-    if best_only:
-        return output[0][1]
-    else:
-        return [i[1] for i in output]
+    return output
 
 def find_window() -> None:
     global _window_rect
+    screenshot = take_screenshot(_screenshoter.monitors[1])
     
-    screenshot = cv2.cvtColor(np.array(ImageGrab.grab()), cv2.COLOR_RGB2BGR)
-    top_left = find_in_image(_top_left_corner_image, screenshot, best_only=True)
-    bottom_right = find_in_image(_bottom_right_corner_image, screenshot, best_only=True)
     
-    if top_left and bottom_right:
+    top_left_corner = find_in_image(_top_left_corner_image, screenshot)
+    bottom_right_corner = find_in_image(_bottom_right_corner_image, screenshot)
+    
+    if top_left_corner and bottom_right_corner:
+        top_left_rect = top_left_corner[0][1]
+        bottom_right_rect = bottom_right_corner[0][1]
+        
         _window_rect = Rectangle(
-            top_left.left,
-            top_left.top,
-            bottom_right.right - top_left.left,
-            bottom_right.bottom - top_left.top
+            top_left_rect.left,
+            top_left_rect.top,
+            bottom_right_rect.right - top_left_rect.left,
+            bottom_right_rect.bottom - top_left_rect.top
         )
         
         #  Coloca o mouse em cada ponta para confirmar que a janela foi encontrada corretamente
@@ -85,23 +96,12 @@ def find_window() -> None:
         push_state('play_game')
 
 def play_game() -> None:
-    screenshot = cv2.cvtColor(
-        np.array(
-            ImageGrab.grab(
-                bbox=(
-                    _window_rect.left,
-                    _window_rect.top,
-                    _window_rect.right,
-                    _window_rect.bottom
-                )
-            )
-        ), 
-        cv2.COLOR_RGB2BGR
-    )
+    screenshot = take_screenshot(_window_rect.absolute_rect())
+    
     #  Encontra os alvos na tela
     targets = find_in_image(_target_image, screenshot, .99)
     
-    for target in targets:
+    for coef, target in targets:
         click(
             (
                 _window_rect.left + target.centerx,
@@ -111,6 +111,7 @@ def play_game() -> None:
 
 def loop() -> None:
     while True:
+        
         function = get_current_state()
         if function is None:
             return
@@ -153,7 +154,7 @@ _bottom_right_corner_image: np.array = cv2.imread("img/bottom_right.png")
 _target_image: np.array = cv2.imread("img/target.png")
 
 _window_rect: Rectangle = None
-
+_screenshoter = mss.mss()
 register_state('find_window', find_window)
 register_state('play_game', play_game)
 push_state('find_window')
